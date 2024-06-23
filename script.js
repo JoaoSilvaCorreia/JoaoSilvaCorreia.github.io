ae77238e855bec445b3199d58ce94f2d78728e65
João Vitor da Silva Correia <joaovitorsilvacorreia@gmail.com>
2024-06-23 03:33:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+<script>
+    document.querySelector('form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        alert('Formulário enviado com sucesso!');
+    });
+</script>
